a3dd0bb6fbfad49200e47159c57a344e8d5e4603
marloBarksdale <lyndenflood@gmail.com>
2022-01-05 19:20:48 -0500

    add context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,18 @@ import * as Si from 'react-icons/si';
 import Searchbar from './Searchbar';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { useTheme } from '../hooks/useTheme';
+
 const Navbar = () => {
+  const { color, changeColor } = useTheme();
   return (
     <div className='navbar'>
-      <nav>
+      <nav
+        style={{ background: color }}
+        onClick={() => {
+          changeColor('red');
+        }}
+      >
         <Link to='/' className='site-name'>
           <h1>
             Library on Firebase <Si.SiFirebase />
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,28 @@
+import { createContext } from 'react';
+import { useReducer } from 'react';
+
+export const ThemeContext = createContext();
+
+const themeReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, color: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export const ThemeProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(themeReducer, { color: 'blue' });
+
+  const changeColor = (color) => {
+    dispatch({ type: 'CHANGE_COLOR', payload: color });
+  };
+
+  return (
+    <ThemeContext.Provider value={{ ...state, changeColor }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
